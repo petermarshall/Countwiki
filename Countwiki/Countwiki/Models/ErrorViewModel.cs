97f48de86fd6a1159ba7ce8e4e80ba9f24c360e0
P33tr <marshall.peter@gmail.com>
2020-12-30 14:30:33 +0000

    created web site

---
+++ Countwiki/Countwiki/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Countwiki.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
