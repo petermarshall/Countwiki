a1a8745ccccee749c317b544ac92cbc758ba6196
P33tr <marshall.peter@gmail.com>
2020-12-30 15:14:30 +0000

    Added HtmlAgilityPack and counter the words

--- Countwiki/Countwiki/Services/WordCounter.cs
+++ Countwiki/Countwiki/Services/WordCounter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,7 +15,28 @@ namespace Countwiki.Services
 
         public int Count (string html)
         {
-            return 1;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // work with the body and ignore the rest
+            var body = htmlDocument.DocumentNode.SelectSingleNode("//body");
+
+            // selecting just text nodes you may get javascript in ther as well.
+            // so "//text()[not(parent::script)]" eliminates the javscript from the count
+            var textNodes = body.SelectNodes("//text()[not(parent::script)]");
+
+            int wordCount = 0;
+            foreach (var textNode in textNodes)
+            {
+
+                var words = textNode.InnerText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                // StringSplitOptions.RemoveEmptyEntries - Omit array elements that contain an empty string from the result.                
+                    
+                int wordsInNode = words.Count();
+
+                wordCount += wordsInNode;
+            }
+            return wordCount;
         }
     }
 }
