7664594ae466fcee67eba819209610d8e2ed5f54
P33tr <marshall.peter@gmail.com>
2020-12-30 14:33:59 +0000

    Added WordCounter Service

---
+++ Countwiki/Countwiki/Services/IWordCounter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Countwiki.Services
+{
+    public interface IWordCounter
+    {
+        int Count(string html);
+    }
+}
---
+++ Countwiki/Countwiki/Services/WordCounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Countwiki.Services
+{
+    public class WordCounter
+    {
+        public WordCounter()
+        {
+
+        }
+
+        public int Count (string html)
+        {
+            return 1;
+        }
+    }
+}
